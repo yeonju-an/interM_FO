89de57fc2d2d3c7918d02d8f66104781aa6df8a9
안연주 <yjan@idevel.kr>
2023-05-24 20:01:54 +0900

    제품 메인 스와이퍼 진행중

--- pc/_markup/js/ui.js
+++ pc/_markup/js/ui.js
@@ -1,21 +1,7 @@
 $(document).ready(function (){
-   // 20230524 안연주 gnb mouseenter/mouseleave
-    $('#gnb').mouseenter(function (){
-        $('#header').addClass('active');
-        $('#gnb-sub-box, .depth-2').stop().slideDown();
-    });
-    $('#header').mouseleave(function (){
-        $('#header').removeClass('active');
-        $('#gnb-sub-box, .depth-2').stop().slideUp();
-    });
-
-    // 20230524 안연주 언어 변경 버튼 mouseenter/mouseleave
-    $('.btn-wrap button.lang').mouseenter(function (){
-        $(this).addClass('active');
-        $('.lang-select').stop().slideDown();
-    });
-    $('.lang-select').mouseleave(function (){
-        $('.btn-wrap button.lang').removeClass('active');
-        $('.lang-select').stop().slideUp();
+    var bannerSwiper = new Swiper("#productMain .bannerSwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
     });
 });
