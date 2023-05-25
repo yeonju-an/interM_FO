90a70686b2a1001e9f360165b7724b6cb5b0d119
안연주 <yjan@idevel.kr>
2023-05-25 17:05:45 +0900

    비즈니스 슬라이드 진행중

--- pc/_markup/js/ui.js
+++ pc/_markup/js/ui.js
@@ -28,4 +28,21 @@ $(document).ready(function (){
         $(this).css('display','none');
         $('.swiper-button-pause').css('display','block');
     })
+
+    // 20230525 안연주 제품 메인 비즈니스 스와이퍼
+    let businessLabel = ['조리 대체 로봇', '셀프 주문 시스템', '자동화 응대','어쩌구','저쩌구'];
+
+    var businessSwiper = new Swiper("#productMain .businessSwiper", {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            renderBullet: function (index, className) {
+                return '<div class="' + className + '">' + (businessLabel[index]) + '</div>';
+            },
+        },
+    });
 });
