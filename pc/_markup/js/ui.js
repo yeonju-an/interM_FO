152fb16e3b2ef22045b5c7c91fc4df807f4a3456
안연주 <yjan@idevel.kr>
2023-05-25 13:57:00 +0900

    제품 메인 스와이퍼 커스텀, 일시정지 기능 추가

--- pc/_markup/js/ui.js
+++ pc/_markup/js/ui.js
@@ -1,7 +1,31 @@
 $(document).ready(function (){
+    // 20230524 안연주 제품 메인 스와이퍼
     var bannerSwiper = new Swiper("#productMain .bannerSwiper", {
+        loop: true,
+        autoplay: {
+            delay: 3000,// animation countingBar와 시간 동일하게
+            disableOnInteraction: false
+        },
+        effect: 'fade',
+        speed: 1000,
+        allowTouchMove: false,
         pagination: {
             el: ".swiper-pagination",
+            renderBullet: function (index, className) {
+                return '<li class="' + className + '">'+ '0' + (index + 1) + "<span class=\"line\"></span></li>";
+            },
         },
     });
+
+    // 20230525 안연주 제품 메인 스와이퍼 일시정지/재생
+    $('.swiper-button-pause').on('click', function(){
+        bannerSwiper.autoplay.stop();
+        $(this).css('display','none');
+        $('.swiper-button-play').css('display','block');
+    })
+    $('.swiper-button-play').on('click', function(){
+        bannerSwiper.autoplay.start();
+        $(this).css('display','none');
+        $('.swiper-button-pause').css('display','block');
+    })
 });
